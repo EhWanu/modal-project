0bd4c430a534cedb9014e6a1ebf32ebc471b1a3e
EhWanu <cameronjshadgett@gmail.com>
2022-02-04 14:44:54 -0500

    29. CHALLENGE - Reusing Components

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,28 @@
 			</template>
 		</Modal>
 	</div>
+
+	<div v-if="showModal2">
+		<Modal theme="" @close="toggleModal2">
+			<h1>This is Modal2</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur
+				adipisicing elit. Facilis esse molestias
+				laboriosam odit laudantium, voluptas,
+				aspernatur hic nam suscipit sunt corporis
+				dolorum aliquam fugit voluptates? Harum ipsam
+				distinctio pariatur molestias?
+			</p>
+			<template v-slot:links>
+				<a href="">Sign Up Now!</a>
+				<a href="">More Info</a>
+			</template>
+		</Modal>
+	</div>
 	<button @click="toggleModal">Open Modal</button>
+	<button @click="toggleModal2">
+		Open Modal2
+	</button>
 </template>
 
 <script>
@@ -33,11 +54,15 @@ export default {
 			header: "Sign up for the Giveaway!",
 			text: "Check out cameronshadgett.com!",
 			showModal: false,
+			showModal2: false,
 		};
 	},
 	methods: {
 		toggleModal() {
 			this.showModal = !this.showModal;
+		},
+		toggleModal2() {
+			this.showModal2 = !this.showModal2;
 		},
 	},
 };
